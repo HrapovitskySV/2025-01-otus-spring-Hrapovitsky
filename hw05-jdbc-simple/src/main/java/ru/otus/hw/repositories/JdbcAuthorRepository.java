39752962af865e4fa053d105baa6a356f1376280
sergey <1Crom@mail.ru>
2025-03-06 22:42:29 +0300

    1 commit

--- hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -13,21 +13,43 @@ import java.util.Optional;
 @Repository
 public class JdbcAuthorRepository implements AuthorRepository {
 
-    @Override
-    public List<Author> findAll() {
-        return new ArrayList<>();
+    private final JdbcOperations jdbc;
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    public PersonDaoJdbc(NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        // Это просто оставили, чтобы не переписывать код
+        // В идеале всё должно быть на NamedParameterJdbcOperations
+        this.jdbc = namedParameterJdbcOperations.getJdbcOperations();
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
     }
 
     @Override
-    public Optional<Author> findById(long id) {
-        return Optional.empty();
+    public List<Author> findAll()
+    {
+        return jdbc.query("select id, full_name from authors", new AuthorRowMapper());
+    }
+
+    @Override
+    public Optional<Author> findById(long id)
+    {
+        Map<String, Object> params = Collections.singletonMap("id", id);
+        try {
+            return namedParameterJdbcOperations.queryForObject(
+                    "select id, full_name from authors where id = :id", params, new AuthorRowMapper()
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            long id = resultSet.getLong("id");
+            String fullName = resultSet.getString("full_name");
+            return new Author(id, fullName);
         }
     }
 }
--- hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -16,11 +16,21 @@ public class JdbcBookRepository implements BookRepository {
 
     @Override
     public Optional<Book> findById(long id) {
+
+
         return Optional.empty();
+
     }
 
     @Override
     public List<Book> findAll() {
+
+        return jdbc.query("select b.id, b.title, b.author_id, b.genre_id, a.full_name as author_name, g.name as genre_name" +
+                "from books as b " +
+                "   left join authors as a " +
+                "       on b.author_id=a.id " +
+                "   left join genres as g " +
+                "       on b.genre_id=g.id", new BookRowMapper());
         return new ArrayList<>();
     }
 
@@ -57,7 +67,23 @@ public class JdbcBookRepository implements BookRepository {
 
         @Override
         public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return null;
+            //return jdbc.query("select b.id, b.title, b.author_id, b.genre_id, a.full_name as author_name, g.name as genre_name" +
+
+            long author_id = resultSet.getLong("author_id");
+            Author author;
+            if (!Objects.isNull(author_id)){
+                author=new Author(author_id, resultSet.getLong("author_name"));
+            }
+            long genre_id = resultSet.getLong("genre_id");
+            Genre genre;
+            if (!Objects.isNull(genre_id)){
+                genre=new Genre(genre_id, resultSet.getLong("genre_name"));
+            }
+
+            long id = resultSet.getLong("id");
+            String title = resultSet.getString("title");
+            return new Book(id, title, author, genre);
+
         }
     }
 }
