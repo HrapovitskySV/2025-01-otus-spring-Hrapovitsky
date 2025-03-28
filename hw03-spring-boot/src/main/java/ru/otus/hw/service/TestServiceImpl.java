ea55fa732c2786d5a9986f3eb5b68696c2ed3639
sergey <1Crom@mail.ru>
2025-03-28 09:41:55 +0300

    1 commit  ДЗ3

---
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,39 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.dao.dto.QuestionToViewConverter;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    private final QuestionToViewConverter questionToViewConverter;
+
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLine("");
+
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var q: questions) {
+            ioService.printLine(questionToViewConverter.convertToView(q));
+
+            int answerNumber = ioService.readIntForRangeWithPromptLocalized(1,q.answers().size(),"TestService.enter.the.response.number","TestService.error.entering.number");
+            testResult.applyAnswer(q, q.answers().get(answerNumber - 1).isCorrect());
+        }
+        return testResult;
+    }
+
+}
