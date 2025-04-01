d7392f1da59460e58145172b3576a30a9486d8a2
sergey <1Crom@mail.ru>
2025-04-01 09:54:32 +0300

    1 commit  ДЗ6

---
+++ hw06-jpa/src/main/java/ru/otus/hw/converters/GenreConverter.java
@@ -0,0 +1,24 @@
+package ru.otus.hw.converters;
+
+import org.springframework.stereotype.Component;
+import ru.otus.hw.models.Genre;
+import ru.otus.hw.models.dto.GenreDto;
+
+@Component
+public class GenreConverter {
+    public String genreToString(Genre genre) {
+        return "Id: %d, Name: %s".formatted(genre.getId(), genre.getName());
+    }
+
+    public String genreDtoToString(GenreDto genre) {
+        return "Id: %d, Name: %s".formatted(genre.getId(), genre.getName());
+    }
+
+    public GenreDto toDto(Genre genre) {
+        var genreDto = new GenreDto();
+        genreDto.setId(genre.getId());
+        genreDto.setName(genre.getName());
+
+        return genreDto;
+    }
+}
