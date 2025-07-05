d85053cb2373cf8d51708e14197a3293872e45a0
sergey <1Crom@mail.ru>
2025-07-05 11:53:48 +0300

    commit 1

--- hw14_spring_batch/src/main/java/ru/otus/hw/processors/CommentItemProcessor.java
+++ hw14_spring_batch/src/main/java/ru/otus/hw/processors/CommentItemProcessor.java
@@ -2,18 +2,9 @@ package ru.otus.hw.processors;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import ru.otus.hw.exceptions.EntityNotFoundException;
-import ru.otus.hw.modelsJpa.Author;
 import ru.otus.hw.modelsJpa.Comment;
-import ru.otus.hw.modelsMongo.AuthorMongo;
 import ru.otus.hw.modelsMongo.CommentMongo;
-import ru.otus.hw.services.CountMapper;
 import ru.otus.hw.services.MapObjectService;
-
-import java.util.Map;
-
-import static java.util.Objects.isNull;
 
 @RequiredArgsConstructor
 public class CommentItemProcessor implements ItemProcessor<CommentMongo, Comment> {
